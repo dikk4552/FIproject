30f10c2d43155c196d6f065e64fa256ade16ece1
다영 송 <oksong07@naver.com>
2022-12-21 01:03:25 +0000

    commit

--- app/webapp/controller/Home.controller.js
+++ app/webapp/controller/Home.controller.js
@@ -1,10 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(
-	Controller
-) {
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("project1.controller.Home", {
+        onInit: function () {
+            const myRoute = this.getOwnerComponent().getRouter().getRoute("Home");
+            myRoute.attachPatternMatched(this.onMyRoutePatterMatched, this);
+        },
+        onMyRoutePatterMatched: async function () {
+            const BP = await $.ajax({
+                type: "get",
+                url: "/bpservice/BP?$filter=BP_payterm eq '0001'"
+            });
+            console.log(BP);
+            let BPModel = new JSONModel(BP.value);
+            this.getView().setModel(BPModel, "BPModel");
+            console.log(this.getView().getModel("BPModel"));
+        },
+		onBP_list: function () {
+            this.getOwnerComponent().getRouter().navTo("BP");
+        },
+        onGL_list: function () {
+            this.getOwnerComponent().getRouter().navTo("GL");
+        },
+		onDoc_list: function () {
+            this.getOwnerComponent().getRouter().navTo("Doc");
+        },
+        onNews: function () {
+            window.open('https://news.sap.com/'); 
+        }
 	});
 });
--- db/doc.cds
+++ db/doc.cds
@@ -1,6 +1,6 @@
 namespace FIproject.doc;
 entity Doc {
-Doc_number : String @title : '전표번호';
+key Doc_number : String @title : '전표번호';
 Doc_docdate : String @title : '증빙일';
 Doc_postdate : String @title : '전기일';
 Doc_cocd : String @title : '회사코드';
@@ -16,4 +16,5 @@ Doc_C_acct : String @title : 'G/L';
 Doc_C_amount : Integer @title : '금액';
 Doc_C_cost : String @title : 'Cost Center';
 Doc_C_prof : String @title : 'Profit Center';
+Doc_duedate : String @title : '만기일';
 }
